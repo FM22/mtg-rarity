da19332ff808dc63ddcaba1fe782465d6b0157ba
FM22 <artem.khovanov@gmail.com>
2020-06-25 16:59:04 +0100

    Started oracle text cleanup

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 import json
+import re
 
 #read and parse the datafile
 with open("data.json", encoding="utf8") as f:
     raw = f.read()
     data = json.loads(raw)
 
+#parse oracle texts
+orTxts = []
 for card in data:
-    if("Creature" in card["type_line"].split(" ") and card["layout"] == "normal"):
-        i += 1
-    j += 1
-print(i)
-print(j)
+    orTxt = card["oracle_text"]
+    orTxt = re.sub("\(.*\)", "", orTxt) #strip out reminder text (text in brackets)
+    orTxt = re.sub("\n", " NEWLINE ", orTxt) #turn newlines into words
+    orTxt = re.sub("\}\{", "} {", orTxt) #space out adjacent mana symbols
+    orTxt = re.sub(":", " CCOLON ", orTxt) #turn colons into words
+    orTxt = re.sub("  ", " ", orTxt) #remove any generated double spaces
+    orTxt = re.sub("\,|\.", "", orTxt) #remove all commas and full stops
+    orTxt = re.sub(card["name"], "CARDNAME", orTxt) #replace instances of self reference
+    orTxt = orTxt.lower() #make everything lowercase
+    print(orTxt)
