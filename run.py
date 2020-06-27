45426166c093c75c6448395276ce9ab383302813
FM22 <artem.khovanov@gmail.com>
2020-06-27 16:11:40 +0100

    Finished data pre processing

--- run.py
+++ run.py
@@ -6,22 +6,23 @@ import unidecode
 with open("data.json", encoding = "utf8") as f:
     data = json.load(f)
 
+# list of input vectors
+# index   value
+# 0       power
+# 1       toughness
+# 2       cmc
+# 3       X spell counter (non-X-spell=0, X=1, XX=2)
+# 4       generic mana
+# 5       hybrid mana
+# 6 to 10 devotion to W-U-B-R-G
+# 11      legendary flag
+# 12      articact flag
+# 13      enchantment flag
+inputVecs = [[]] * len(data)
+
+inputWords = [] #list of lists of words
 ptDict = {"*": "0", "1+*": "1"} #converts */* creatures and Goyf
 
-orTxts = []
-powers = []
-toughnesses = []
-cmcs = []
-xs = [0] * len(data)
-gens = xs[:] #shallow copy
-hybs = xs[:]
-ws = xs[:]
-us = xs[:]
-rs = xs[:]
-bs = xs[:]
-gs = xs[:]
-i = 0 #for loop counter
-v = 0;
 #extract data
 for card in data:
     #parse oracle text
@@ -39,26 +40,25 @@ for card in data:
     orTxt = re.sub("^ newline", "", orTxt) #remove leading newlines
     orTxt = re.sub("( $|^ )", "", orTxt) #remove leading and trailing spaces
     orTxt = orTxt.lower() #make everything lowercase
-    orTxts.append(orTxt)
-    if orTxt == "": v += 1
+    inputWords.append(orTxt.split(" ")) #split into word tokens
 
-    #parse P/T
-    powers.append(int(ptDict.get(card["power"], card["power"])))
-    toughnesses.append(int(ptDict.get(card["toughness"], card["toughness"])))
-
-    cmcs.append(card["cmc"]) #parse cmc
-
-    #parse manacost
-    for char in card["mana_cost"]:
-        if char == "X": xs[i] += 1
-        if char == "W": ws[i] += 1
-        if char == "U": us[i] += 1
-        if char == "B": bs[i] += 1
-        if char == "R": rs[i] += 1
-        if char == "G": gs[i] += 1
-        if char == "/": hybs[i] += 1 #hybrid mana eg {W/U}
-        if char in [str(i) for i in range(0,99)]: gens[i] += int(char) #generic mana
-    i += 1
-
-
-print(v)
+    #parse numeric data
+    types = card["type_line"].split(" ") #get card types
+    mana = card["mana_cost"]
+    genNums = re.findall("[0-9]", mana) #get generic component of mana cost (None if no generic component)
+    inputVec = [int(ptDict.get(card["power"], card["power"])),
+                int(ptDict.get(card["toughness"], card["toughness"])),
+                int(card["cmc"]),
+                sum([int(i) for i in genNums]), #parse generic mana cost
+                mana.count("X"), #parse mana stats
+                mana.count("/"),
+                mana.count("W"),
+                mana.count("U"),
+                mana.count("B"),
+                mana.count("R"),
+                mana.count("G"),
+                1 if "Legendary" in types else 0, #parse type flags
+                1 if "Artifact" in types else 0,
+                1 if "Enchantment" in types else 0]
+    inputVecs.append(inputVec)
+    print(card["name"] + ": " + str(inputVec))
